0a48d48d11b56acae4768cd5997b9da70897794a
Justice Ngwenya <justice@annotations.co.za>
2020-10-18 14:18:10 +0200

    Tournament Tracker Library Tournament Viewer Model

---
+++ TrackerLibrary/MatchupEntryModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerLibrary
+{
+    /// <summary>
+    /// Represents one team in a Matchup
+    /// </summary>
+    public class MatchupEntryModel
+    {
+        /// <summary>
+        /// Represent one team in the Matchup.
+        /// </summary>
+        public TeamModel TeamCompeting { get; set; }
+        /// <summary>
+        /// Represents the score for this particalar team.
+        /// </summary>
+        public double Score { get; set; } 
+        /// <summary>
+        /// Represents the matchup that this team 
+        /// came from as the winner.
+        /// </summary>
+        public MatchupModel ParentMatchup { get; set; }
+
+    }
+}
---
+++ TrackerLibrary/PersonModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerLibrary
+{
+    /// <summary>
+    /// Represents one Person
+    /// </summary>
+    public class PersonModel
+    {
+        /// <summary>
+        /// The first name of the person
+        /// </summary>
+        public string FirstName { get; set; }
+        /// <summary>
+        /// The last name of the person
+        /// </summary>
+        public string lastName { get; set; }
+        /// <summary>
+        /// The primary email address of the person
+        /// </summary>
+        public string EmailAddress { get; set; }
+        /// <summary>
+        /// the primary cell phone number of the person
+        /// </summary>
+        public string CellphoneNumber { get; set; }
+    }
+}
